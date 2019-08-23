a7d0f3cba7e1126023263a440e9e8767ab26e607
Anand pratap singh <47928185+singhanand453@users.noreply.github.com>
2019-08-23 22:36:51 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,7 +2,7 @@ from tkinter import *
 window=Tk()
 window.title("1ST GUI")
 window.geometry("300x200")
-label=Label(window,text="hii,i am TAPS",fg="red",bg="yellow")
+label=Label(window,text="hii,i am ........",fg="red",bg="yellow")
 label.pack(side=TOP)
 button=Button(window,text="click button",fg="red",bg="gray")
 button.pack()
